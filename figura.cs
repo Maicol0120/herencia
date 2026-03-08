354fe6738302658e4281be8838539b2663c181ae
Maicol0120 <maicolestivilla2004@gmail.com>
2026-03-08 07:50:00 -1200

    Add abstract class Figura for area and perimeter calculations

---
+++ figura.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TallerHerencia
+{
+    public abstract class Figura
+    {
+        public abstract double CalcularArea();
+        public abstract double CalcularPerimetro();
+    }
+}
